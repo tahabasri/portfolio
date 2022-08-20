f5cfad978f05dc487490ca1bbb9a9f2e08630783
Taha Basri <tahabsri@gmail.com>
2022-08-21 00:32:05 +0100

    Update portfolio data and UI

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,7 @@ window.onscroll = function () {
             $("#header").css("color", "#fff");
             $("#logo").css("color", "#fff");
             $("#header").css("box-shadow", "0px 0px 20px rgba(0,0,0,0.09)");
-            $("#header").css("padding", "4vh 4vw");
+            $("#header").css("padding", "2vh 4vw");
             $("#navigation a").hover(function () {
                 $(this).css("border-bottom", "2px solid rgb(255, 44, 90)");
             }, function () {
@@ -17,7 +17,7 @@ window.onscroll = function () {
             $("#header").css("color", "#000");
             $("#logo").css("color", "rgb(239, 75, 75)");
             $("#header").css("box-shadow", "0px 0px 0px rgba(0,0,0,0)");
-            $("#header").css("padding", "6vh 4vw");
+            $("#header").css("padding", "2vh 4vw");
             $("#navigation a").hover(function () {
                 $(this).css("border-bottom", "2px solid #fff");
             }, function () {
